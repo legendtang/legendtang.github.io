0ae9f9946999237414e85d86157fb1909406b2d0
Legend Tang <sp3478@gmail.com>
2015-04-17 23:43:40 +0800

    eggs possibly fix

--- eggs.js
+++ eggs.js
@@ -1,4 +1,4 @@
-console.log("%c", "padding:6px 150px; line-hight: 28px; background:url('./images/Konami_Code.png') no-repeat;");
+console.log("%c", "padding:6px 150px; line-hight: 28px; background:url('http://legendt.me/images/Konami_Code.png') no-repeat;");
 
 $(document).ready(function(){
 	$("#outside" ).on( "click",function() {
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	});
 });
 
-konami = new Konami()
+konami = new Konami();
 konami.code = function(){
 	console.log("打开了新世界的大门<br />");
 	console.log("Across the Great Wall we can reach every corner in the world.");
