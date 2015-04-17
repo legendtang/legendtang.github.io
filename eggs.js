612a22979ae4f57c7918ecb3ac2fe834376f1a77
Legend Tang <sp3478@gmail.com>
2015-04-17 23:37:04 +0800

    Domains change for blogs and eggs update

--- eggs.js
+++ eggs.js
@@ -1,8 +1,8 @@
-console.log("KONAMI Gamers Find the Passphrase");
+console.log("%c", "padding:6px 150px; line-hight: 28px; background:url('./images/Konami_Code.png') no-repeat;");
 
 $(document).ready(function(){
 	$("#outside" ).on( "click",function() {
-		window.location.href='http://blog.legendt.me';
+		window.location.href='https://blog.legendt.com';
 	});
 });
 
@@ -12,7 +12,7 @@ konami.code = function(){
 	console.log("Across the Great Wall we can reach every corner in the world.");
 
 	$( "#inside" ).on( "click",function() {
-		window.location.href='http://jtapi.legendt.me';
+		window.location.href='https://drrr.legendt.com';
 	});
 }
 
